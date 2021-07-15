54a8b0d246d6bbd15abf22f9b7f210220a805611
TheFakeCake <TheFakeCake@users.noreply.github.com>
2021-07-15 07:58:05 +0200

    Supprime l'initialisation manuel de Bootstrap collapse (inutile)

--- assets/app.js
+++ assets/app.js
@@ -3,12 +3,6 @@ import './styles/app.scss';
 import Collapse from 'bootstrap/js/dist/collapse';
 import { initOnScrollAnimations } from './js/animate-on-scroll';
 
-// Executing Bootstrap's collapse javascript module
-var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'));
-var collapseList = collapseElementList.map(function (collapseEl) {
-    return new Collapse(collapseEl, { toggle: false })
-});
-
 // Initializing the animations on scroll if not on mobile
 if (!window.isMobile) {
     initOnScrollAnimations();
